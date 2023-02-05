f14d2e6bf5302a258ca55bb5e4d2e0aae0bb0e5a
Pascal <lac900@live.ca>
2023-02-04 22:46:36 -0500

    PC Ajout map de base plus server communique avec client et envoie certaine data

--- Server/server.js
+++ Server/server.js
@@ -1,11 +1,27 @@
-var http = require('http'); 
+var http = require('http');
+var fs = require('fs');
+var readline = require("readline");
+
+
 
 var server = http.createServer(function (req, res) {   
    
     if (req.url == '/HW') {
-            res.writeHead(200, { 'Content-Type': 'application/json' });
-            res.write(JSON.stringify({ message: "Hello World"}));  
-            res.end();  
+            res.writeHead(200, { 'Content-Type': 'application/json',
+            "Access-Control-Allow-Origin": "*" });
+            const stream = fs.createReadStream('../clear_data/data/actes-criminels.csv');
+            const rl = readline.createInterface({ input: stream });
+            let data = [];
+            rl.on("line", (row) => {
+                // let t = row.split(",");
+                // console.log(t[0]);
+                data.push(row.split(","));
+            });
+             
+            rl.on("close", () => {
+                res.write(JSON.stringify({ message: data}));  
+                res.end();
+            });
     }
     if (req.url == '/Police') {
             res.writeHead(200, { 'Content-Type': 'application/json' });
@@ -13,6 +29,25 @@ var server = http.createServer(function (req, res) {
             res.end();  
     }
 });
+
+function printHW(){
+    const stream = fs.createReadStream('../clear_data/data/actes-criminels.csv');
+    const rl = readline.createInterface({ input: stream });
+    let data = [];
+    rl.on("line", (row) => {
+        // let t = row.split(",");
+        // console.log(t[0]);
+        data.push(row.split(","));
+    });
+     
+    rl.on("close", () => {
+        console.log(JSON.stringify({ message: data}));
+    });
+    
+}
+
+
+
 
 server.listen(5000);
 
